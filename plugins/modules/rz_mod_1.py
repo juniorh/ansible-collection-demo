44be32682990f98288fb824d1342351e46cc75cd
juniorh <ri24l_pwt@yahoo.com>
2021-04-25 04:52:11 +0700

    fix sanity test

--- plugins/modules/rz_mod_1.py
+++ plugins/modules/rz_mod_1.py
@@ -5,6 +5,7 @@
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import (absolute_import, division, print_function)
+
 __metaclass__ = type
 
 ANSIBLE_METADATA = {
@@ -13,55 +14,56 @@ ANSIBLE_METADATA = {
     'supported_by': 'community'
 }
 
-DOCUMENTATION = r'''
+DOCUMENTATION = '''
 ---
-module: rz_mod_1.oy
+module: rz_mod_1
 short_description: Test creating ansible module, just do echo
-description
-  - Echo json input
-  - Description 2
-  - Description 3
+description:
+    - Echo json input
+    - Description 2
+    - Description 3
 requirements:
-  - python >= 3.0
+    - python >= 3.0
+author: "ri24l_pwt@yahoo.com (@juniorh)"
 options:
-  custom_opt_1:
-    description: 
-      - This is a description of custom_opt_1
-      - Additional description
-    type: bool
-    required: true
-  custom_opt_2:
-    description:
-      - This is a description of custom_opt_2
-    type: str
-    required: true
-  custom_opt_3:
-    descipriotn: This is a description of custom_opp_3
-    type: int
-  custom_opt_4:
-    description: This is a description of custom_app_4
-    type: str
-    choise: [present, absent]
-    default: present
-  custom_opt_5:
-    description: This is a description of custom_app_5
-    type: dict
-    required: false
-  custom_opt_6:
-    description: This is a description of custom_app_6
-    type: dict
-    suboptions:
-      sub_1:
-        description: This is a description (str) of sub-dic of custom_app_6
+    custom_opt_1:
+        description:
+          - This is a description of custom_opt_1
+          - Additional description
+        type: bool
+        required: true
+    custom_opt_2:
+        description:
+          - This is a description of custom_opt_2
         type: str
         required: true
-      sub_2:
-        description: This is a description (int) of sub-dic of custom_app_6
+    custom_opt_3:
+        description: This is a description of custom_opp_3
         type: int
+    custom_opt_4:
+        description: This is a description of custom_app_4
+        type: str
+        choices: [present, absent]
+        default: present
+    custom_opt_5:
+        description: This is a description of custom_app_5
+        type: dict
         required: false
+    custom_opt_6:
+        description: This is a description of custom_app_6
+        type: dict
+        suboptions:
+          sub_1:
+              description: This is a description (str) of sub-dic of custom_app_6
+              type: str
+              required: true
+          sub_2:
+              description: This is a description (int) of sub-dic of custom_app_6
+              type: int
+              required: false
 notes:
-  - this is note 1
-  - this is note 2
+    - this is note 1
+    - this is note 2
 '''
 
 EXAMPLES = r'''
@@ -86,28 +88,28 @@ output_1:
   returned: success
   type: str
   sample: "Sample of output_1"
-output_2
+output_2:
   description: output-type-int
   returned: success
   type: int
   sample: 321
-output_3
+output_3:
   description: output-type-list
   returned: success
   type: list
-output_4
+output_4:
   description: output-type-dict
   returned: success
   type: dict
-output_5
+output_5:
   description: output-type-complex
   returned: success
-  type: str
+  type: complex
   contains:
     o_sub_1:
       description: This is an output (str) of output_5.o_sub_1
       returned: success
-      type: string
+      type: str
     o_sub_2:
       description: This is an output (int) of output_5.o_sub_2
       returned: success
@@ -116,23 +118,30 @@ output_5
 
 from ansible.module_utils.basic import AnsibleModule
 
+
 def run_module():
-  module = AnsibleModule(
-    argument_spec=dict(
-      custom_opt_1 = dict(type='bool', required=True),
-      custom_opt_2 = dict(type='str', required=True),
-    ),
-    supports_check_mode=True,
-  )
-  result = {
-    'changed': True,
-    'msg1': "output-m-1",
-    'msg2': "output-m-2",
-  }
-  module.exit_json(changed=True)
+    module = AnsibleModule(
+        argument_spec=dict(
+            custom_opt_1=dict(type='bool', required=True),
+            custom_opt_2=dict(type='str', required=True),
+            custom_opt_3=dict(type='int', required=False),
+            custom_opt_4=dict(type='str', required=False, default='present', choices=['absent', 'present']),
+            custom_opt_5=dict(type='dict', required=False),
+            custom_opt_6=dict(type='dict', required=False),
+        ),
+        supports_check_mode=True,
+    )
+    result = {
+        'changed': True,
+        'msg1': "output-m-1",
+        'msg2': "output-m-2",
+    }
+    module.exit_json(changed=True)
+
 
 def main():
-  run_module()
+    run_module()
+
 
 if __name__ == '__main__':
-  main()
+    main()
